d98fdc97f1cbef6a7179395bb2e73c39d2a1955d
davidhiroki.ortelt <davidhiroki.ortelt@stud.uni-goettingen.de>
2023-11-26 19:59:11 +0100

    resnet_test

--- pose_estimation/ResNet.py
+++ pose_estimation/ResNet.py
@@ -1,3 +1,12 @@
 import torch as th
 
-print("hello2wwsw")
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+
+def some_function():
+    model.eval()
+    input_data = torch.randn(1, 3, 224, 224)
+    output = model(input_data)
+    print(output)
+
+
+some_function()
