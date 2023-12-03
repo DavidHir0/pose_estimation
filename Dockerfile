d2b2fe793b060e14fc64aa28ee93a141fafc2e0b
DavidHir0 <david@ortelt.org>
2023-12-03 23:02:30 +0100

    data_loading

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,14 @@ ADD . /src
 
 WORKDIR /src
 
+RUN rm /etc/apt/sources.list.d/cuda.list
+RUN rm /etc/apt/sources.list.d/nvidia-ml.list
+RUN apt-key del 7fa2af80
+RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/7fa2af80.pub
+RUN apt-get update
+RUN apt-get install -y ffmpeg
+
 RUN pip install --no-cache-dir --upgrade pip
 RUN pip install -r requirements.txt
 RUN pip install --no-cache-dir git+https://github.com/sinzlab/propose.git
--- pose_estimation/data_pp/data_pp.py
+++ pose_estimation/data_pp/data_pp.py
@@ -1,2 +1,61 @@
 import propose.preprocessing.rat7m as pp
 from propose.datasets.rat7m.loaders import load_mocap, load_cameras
+from pathlib import Path
+import pickle
+
+dirname = "data/rat7m"
+data_key = "s2-d1"
+mocap_path = f"{dirname}/mocap/mocap-{data_key}.mat"
+
+# Convert video to images
+#pp.convert_movies_to_images(dirname, data_key)
+
+
+# Load pose and camera
+mocap = load_mocap(mocap_path)
+cameras = load_cameras(mocap_path)
+
+# Mask
+nan_mask = pp.mask_nans(mocap)
+fail_mask = pp.mask_marker_failure(mocap)
+
+mask = nan_mask + fail_mask
+
+mocap, cameras = pp.apply_mask(mask, mocap, cameras)
+
+
+
+
+# Save
+pose_dir = Path(f"{dirname}/{data_key}/poses")
+pose_dir.mkdir(parents=True, exist_ok=True)
+
+pose_path = pose_dir / f"{data_key}.npy"
+
+mocap.save(pose_path)
+
+
+
+camera_dir = Path(f"{dirname}/{data_key}/cameras")
+camera_dir.mkdir(parents=True, exist_ok=True)
+
+camera_path = camera_dir / f"{data_key}.pickle"
+
+with open(camera_path, "wb") as f:
+    pickle.dump(cameras, f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
